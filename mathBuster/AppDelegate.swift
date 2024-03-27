c2c762c86d7fe7698b73ff61f1d8c6bef1a83d6a
Nurdaulet <ndauletkhan2004@mail.ru>
2024-03-27 15:38:05 +0500

    First commit

---
+++ mathBuster/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  mathBuster
+//
+//  Created by Нурдаулет Даулетхан on 11.03.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ mathBuster/ScoreLabel.swift
@@ -0,0 +1,13 @@
+//import UIKit
+//
+//class ScoreLabel: UILabel{
+//    override init(frame: CGRect){
+//        super.init(frame: frame)
+//        self.textColor = .myGreen
+//        self.text = "Score: 0"
+//        self.font = .systemFont(ofSize: 20)
+//    }
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
